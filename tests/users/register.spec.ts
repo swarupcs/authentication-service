13b09c173aeba8dc071fed7031e1618aecd7fe52
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-10-12 19:36:58 +0530

    add email sanitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -164,7 +164,7 @@ describe("POST /auth/register", () => {
         it("should return 400 status code if email field is missing", async () => {
             // Arrange
             const userData = {
-                firstName: "Rakesh",
+                firstName: "swarup",
                 lastName: "K",
                 email: "",
                 password: "secret",
@@ -179,6 +179,26 @@ describe("POST /auth/register", () => {
             const userRepository = connection.getRepository(User);
             const users = await userRepository.find();
             expect(users).toHaveLength(0);
+        });
+    });
+
+    describe("Fields are not in proper format", () => {
+        it("should trim the email field", async () => {
+            // Arrange
+            const userData = {
+                firstName: "swarup",
+                lastName: "K",
+                email: " swarup@mern.space ",
+                password: "secret",
+            };
+            // Act
+            await request(app).post("/auth/register").send(userData);
+
+            // Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            const user = users[0];
+            expect(user.email).toBe("swarup@mern.space");
         });
     });
 });
