7009f981250204ca44f701e0b465982a06ac59e5
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-10-19 13:19:33 +0530

    update tenant

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -18,4 +18,8 @@ router.post("/", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
     tenantController.create(req, res, next),
 );
 
+router.patch("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+    tenantController.update(req, res, next),
+);
+
 export default router;
