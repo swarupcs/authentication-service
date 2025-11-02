e340a0356714354aad92c104a445fc8107f376ed
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-11-02 20:41:28 +0530

    improve error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import "reflect-metadata";
 
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import logger from "./config/logger";
-import { HttpError } from "http-errors";
 import authRouter from "./routes/auth";
 import tenantRouter from "./routes/tenant";
 import userRouter from "./routes/user";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 const app = express();
 app.use(
@@ -29,19 +28,6 @@ app.get("/", async (req, res) => {
 app.use("/auth", authRouter);
 app.use("/tenants", tenantRouter);
 
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message);
-    const statusCode = err.statusCode || err.status || 500;
-    res.status(statusCode).json({
-        errors: [
-            {
-                type: err.name,
-                msg: err.message,
-                path: "",
-                location: "",
-            },
-        ],
-    });
-});
+app.use(globalErrorHandler);
 
 export default app;
